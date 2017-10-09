a74d75485803a681df5aacdabfd96a339cde433c
Kathryn Saldivar <kisaldivar1995@gmail.com>
2017-10-08 20:53:02 -0700

    Adding my function for pulling data

--- WebsiteLayout/getData.php
+++ WebsiteLayout/getData.php
@@ -11,8 +11,29 @@ if (mysqli_connect_error())
 $msyqlStatment = "SELECT pen,notch,tag,date1,boar1,date2,boar2,heat,date3,line,newPen,comm FROM pigpens";
 
 // Kathryn's code here
-
-
+function fetchData(){
+	$rowData = $con->query(msyqlStatment);
+	if($rowData->num_rows > 0) {
+		// output data into table
+		while($row = $rowData->fetch_assoc())
+		{
+			echo 
+			"<tr>
+			<td><div contenteditable>".$row["pen"]."</div></td>
+			<td><div contenteditable>".$row["notch"]."</div></td>
+			<td><div contenteditable>".$row["tag"]."</div></td>
+			<td><div contenteditable>".$row["date1"]."</div></td>
+			<td><div contenteditable>".$row["boar1"]."</div></td>
+			<td><div contenteditable>".$row["date2"]."</div></td>
+			<td><div contenteditable>".$row["boar2"]."</div></td>
+			<td><div contenteditable>".$row["heat"]."</div></td>
+			<td><div contenteditable>".$row["date3"]."</div></td>
+			<td><div contenteditable>".$row["line"]."</div></td>
+			<td><div contenteditable>".$row["newPen"]."</div></td>
+			<td><div contenteditable>".$row["comm"]."</div></td>
+			</tr>";
+		}
+	}
 
 // ----------------------------
 // close connection to database

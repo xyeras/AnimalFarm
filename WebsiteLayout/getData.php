ea3f7b83ac7179c91f8edaf1b3aab0f4c38b9123
Kathryn Saldivar <kisaldivar1995@gmail.com>
2017-10-10 17:02:15 -0700

    Fixed Error when Retrieving Data

--- WebsiteLayout/getData.php
+++ WebsiteLayout/getData.php
@@ -12,9 +12,25 @@ $msyqlStatment = "SELECT pen,notch,tag,date1,boar1,date2,boar2,heat,date3,line,n
 
 // Kathryn's code here
 //function fetchData(){
-$rowData = $con->query(msyqlStatment);
+$rowData = $con->query($msyqlStatment);
 if($rowData->num_rows > 0) {
 	// output data into table
+	echo "
+    	<table>
+          <tr>
+	        <th>Pen</th>
+	        <th>Notch</th>
+	        <th>Tag</th>
+        	<th>Date</th>
+        	<th>Boar</th>
+        	<th>Date</th>
+        	<th>Boar</th>
+        	<th>Heat</th>
+        	<th>Date</th>
+        	<th>Line</th>
+        	<th>New Pen</th>
+    	    <th>Comments</th>
+          </tr>";
 	while($row = $rowData->fetch_assoc())
 	{
 		echo 
@@ -32,8 +48,10 @@ if($rowData->num_rows > 0) {
 		<td>".$row["newPen"]."</td>
 		<td>".$row["comm"]."</td>
 		</tr>";
-		}
+	}
+	echo "</table>";
 }
+//}
 
 // ----------------------------
 // close connection to database

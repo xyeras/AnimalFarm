52bb7a8d20049b039a42863da0b9d0e39945c7c1
Kathryn Saldivar <kisaldivar1995@gmail.com>
2017-10-22 20:21:27 -0700

    Base code for adding rows to database

---
+++ addData.js
@@ -0,0 +1,45 @@
+
+function addNew(p,n,t,d1,b1,d2,b2,h,d3,l,nP,c)
+{
+
+	$.post("addData.php",
+	{	pen		: p,
+		notch	: n,
+		tag		: t,
+		date1	: d1,
+		boar1	: b1,
+		date2	: d2,
+		boar2	: b2,
+		heat	: h,
+		date3	: d3,
+		line	: l,
+		newPen	: nP,
+		comm	: c	
+	},
+	function(status){
+		$('#result').html(status);
+	});
+}
+
+// Makes sure the document is ready to be saved and sent
+$(document).ready(function(){
+	
+	//Gets the input from the html file when add button is pressed
+	$('#add').click(function{
+		var p 	= $('#pen').val();
+		var n 	= $('#notch').val();
+		var t 	= $('#tag').val();
+		var d1	= $('#date1').val();
+		var b2	= $('#boar1').val();
+		var d2	= $('#date2').val();
+		var b2	= $('#boar2').val();
+		var h 	= $('#heat').val();
+		var d3	= $('#date3').val();
+		var l 	= $('#line').val();
+		var nP	= $('#newPen').val();
+		var c 	= $('#comm').val();
+		
+		addNew(p,n,t,d1,b1,d2,b2,h,d3,l,nP,c);
+	});
+	
+});
---
+++ addData.php
@@ -0,0 +1,40 @@
+<?php
+
+// connects to the database in 000webhost
+$con = mysqli_connect("localhost","id2929022_150students","csci150","id2929022_pigpens");
+
+// sends out an error message if the connection doesn't happen
+if (mysqli_connect_error())
+{
+	echo "Fail to connect to MySQL:" . mysqli_connect_error();
+}
+
+		
+$pen	=$_POST['pen'];
+$notch	=$_POST['notch'];
+$tag	=$_POST['tag'];
+$date1	=$_POST['date1'];
+$boar1	=$_POST['boar1'];
+$date2	=$_POST['date2'];
+$boar2	=$_POST['boar2'];
+$heat	=$_POST['heat'];
+$date3	=$_POST['date3'];
+$line	=$_POST['line'];
+$newPen	=$_POST['newPen'];
+$comments=$_POST['comm'];
+
+$insert = "INSERT INTO pigpens (pen, notch, tag, date1, boar1, date2, boar2, heat, date3, line, newPen, comm)
+		VALUES ($pen, $notch, $tag, $date1, $boar1, $date2, $boar2, $heat, $date3, $line, $newPen, $comments)";
+
+if($con->query($insert)){
+	echo "alert(\"Save successful!\")";
+}
+else{
+	echo "alert(\"Failed to save data.\")";
+}
+		
+
+// ----------------------------
+// close connection to database
+mysqli_close($con);
+?>

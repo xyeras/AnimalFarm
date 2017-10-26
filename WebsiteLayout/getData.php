01b29fa4722942e118433651ad319992743a22cc
speedy1096 <chrisgonzo@mail.fresnostate.edu>
2017-10-26 16:57:14 -0700

    Add files via upload
    changed ordering of data in getData.php to print in num_rows descending ordering -Chris G.

--- WebsiteLayout/getData.php
+++ WebsiteLayout/getData.php
@@ -8,7 +8,7 @@ if (mysqli_connect_error())
 	echo "Fail to connect to MySQL:" . mysqli_connect_error();
 }
 // statment to retrieve rows from database
-$msyqlStatment = "SELECT pen,notch,tag,date1,boar1,date2,boar2,heat,date3,line,newPen,comm FROM pigpens";
+$msyqlStatment = "SELECT pen,notch,tag,date1,boar1,date2,boar2,heat,date3,line,newPen,comm FROM pigpens ORDER BY num_rows DESC";
 
 // Kathryn's code here
 //function fetchData(){
@@ -16,7 +16,21 @@ $rowData = $con->query($msyqlStatment);
 if($rowData->num_rows > 0) {
 	// output data into table
 	echo "
-    	<table>";
+    	<table>
+          <tr>
+	        <th>Pen</th>
+	        <th>Notch</th>
+	        <th>Tag</th>
+        	<th>Date</th>
+        	<th>Boar</th>
+        	<th>Date</th>
+        	<th>Boar</th>
+        	<th>Heat</th>
+        	<th>Date</th>
+        	<th>Line</th>
+        	<th>New Pen</th>
+    	    <th>Comments</th>
+          </tr>";
 	while($row = $rowData->fetch_assoc())
 	{
 		echo 

206e44e9770f7ffdb74451102b20f96f2a7e35a9
Kathryn Saldivar <kisaldivar1995@gmail.com>
2017-11-14 17:08:39 -0800

    back end for showing row on add more data

---
+++ WebsiteLayout/getRow.js
@@ -0,0 +1,15 @@
+// Makes sure the document is ready to be saved and sent
+$(document).ready(function(){
+	
+	//Gets the row number sent from the edit data page
+	var rn = window.location.search.substring(8);
+	//Takes the row sent from Edit Data, gets the matching row from the Database, 
+	$.post("getRow.php",
+	{
+		rowNum: rn
+	},
+	function(data){
+		$('#rowData').html(data);
+	});
+	
+});

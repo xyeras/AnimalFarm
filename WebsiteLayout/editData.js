ab925ba26545287a0f5f790737fc0b96ab1efba2
xyeras <saika_fujimoto@mail.fresnostate.edu>
2017-12-03 14:00:01 -0800

    Moved file and removed duplicates

--- WebsiteLayout/editData.js
+++
@@ -1,8 +0,0 @@
-function changeInput(x) {
-        var val = $(x).attr('value');
-        alert(val);
-    	document.getElementById('submit').classList = "container center waves-effect waves-light btn-large red";
-        $(x).addClass('selected').siblings().removeClass('selected');
-        document.getElementById('rowNum').value = val;
-}
-

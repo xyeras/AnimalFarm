54b643d1ebeac1885e5385f327e8cbf731a0e7b7
speedy1096 <chrisgonzo@mail.fresnostate.edu>
2018-02-08 20:24:18 -0800

    Add files via upload
    updated updateDB_1.php for new edit page -Chris G.

--- Animal Farm Website/updateDB_1.php
+++ Animal Farm Website/updateDB_1.php
@@ -10,40 +10,40 @@ if (mysqli_connect_error())
 
 // retrieves values from updateDB.js
 $num_rows = mysqli_real_escape_string($con,$_POST['nr']);
-$date2_month = mysqli_real_escape_string($con,$_POST['date2_month']);
-$date2_day = mysqli_real_escape_string($con,$_POST['date2_day']);
-$date2_year = mysqli_real_escape_string($con,$_POST['date2_year']);
-$boar2 = mysqli_real_escape_string($con,$_POST['b2']);
-$date3_month = mysqli_real_escape_string($con,$_POST['date3_month']);
-$date3_day = mysqli_real_escape_string($con,$_POST['date3_day']);
-$date3_year = mysqli_real_escape_string($con,$_POST['date3_year']);
+$pen      = mysqli_real_escape_string($con,$_POST['p']);
+$notch    = mysqli_real_escape_string($con,$_POST['n']);
+$tag      = mysqli_real_escape_string($con,$_POST['t']);
+$breed    = mysqli_real_escape_string($con,$_POST['b']);
+$boar1    = mysqli_real_escape_string($con,$_POST['b1']);
+$boar2    = mysqli_real_escape_string($con,$_POST['b2']);
+$heat     = mysqli_real_escape_string($con,$_POST['h']);
 $line = mysqli_real_escape_string($con,$_POST['line']);
 $newPen = mysqli_real_escape_string($con,$_POST['np']);
 $comm = mysqli_real_escape_string($con,$_POST['comm']);
 
 // --------------- date2 ------------------------------------------
 // converts date from string to int
-$m2Int = (int) $date2_month;
-$d2Int = (int) $date2_day;
-$y2Int = (int) $date2_year ;
+//$m2Int = (int) $date2_month;
+//$d2Int = (int) $date2_day;
+//$y2Int = (int) $date2_year ;
 
 // n is for month with no 0 prefix,j is for the day with no 0 prefix Y is needed for fout digit year
 // this is a string
-$date2 = date("n/j/Y",mktime(0,0,0,$m2Int,$d2Int,$y2Int));
+//$date2 = date("n/j/Y",mktime(0,0,0,$m2Int,$d2Int,$y2Int));
 
 // ---------------- date 3 ----------------------------------------
 // converts date from string to int
-$m3Int = (int) $date3_month;
-$d3Int = (int) $date3_day;
-$y3Int = (int) $date3_year ;
+//$m3Int = (int) $date3_month;
+//$d3Int = (int) $date3_day;
+//$y3Int = (int) $date3_year ;
 
 // n is for month with no 0 prefix,j is for the day with no 0 prefix Y is needed for fout digit year
 // this is a string
-$date3 = date("n/j/Y",mktime(0,0,0,$m3Int,$d3Int,$y3Int));
+//$date3 = date("n/j/Y",mktime(0,0,0,$m3Int,$d3Int,$y3Int));
 
 // ------------------------------------------------------------------
 // update database sentence
-$msyqlStatment = "UPDATE pigpens SET date2 = '$date2', boar2 = '$boar2', date3 = '$date3', line = '$line', newPen = '$newPen', comm = '$comm' WHERE  num_rows = '$num_rows'";
+$msyqlStatment = "UPDATE pigpens SET pen = '$pen', notch ='$notch', tag = '$tag', breed = '$breed', boar1 = '$boar1', boar2 = '$boar2', heat = '$heat', line = '$line', newPen = '$newPen', comm = '$comm' WHERE  num_rows = '$num_rows'";
 
 // update database statement
 if($con->query($msyqlStatment))
